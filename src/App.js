afffd782bac28ad5795982c5c6d63342f054bb9f
Elvis Serrão <elvis.serrao1@gmail.com>
2020-04-28 03:20:56 -0300

    Change how the components are imported

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Cards, Chart, CountryPicker } from './components';
 
 function App() {
   return (
     <div>
       <h1>App</h1>
+      <Cards />
+      <Chart />
+      <CountryPicker />
     </div>
   );
 }
