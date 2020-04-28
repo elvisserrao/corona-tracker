d987c66d06ac8d8f4f9e4d46bd57c6c72f4cd437
Elvis Serrão <elvis.serrao1@gmail.com>
2020-04-28 04:16:06 -0300

    Fetch data

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import { Cards, Chart, CountryPicker } from './components';
 import styles from './App.module.css';
+import fetchData from './api';
 
 class App extends React.PureComponent {
+  async componentDidMount() {
+    const data = await fetchData();
+    console.log(data);
+  }
+
   render() {
     return (
       <div className={styles.container}>
