42538609adec7faacb230555462264e5d6757324
Elvis Serrão <elvis.serrao1@gmail.com>
2020-04-28 03:36:19 -0300

    Convert functional component into class component

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import React from 'react';
 import { Cards, Chart, CountryPicker } from './components';
 import styles from './App.module.css';
 
-function App() {
-  return (
-    <div className={styles.container}>
-      <Cards />
-      <Chart />
-      <CountryPicker />
-    </div>
-  );
+class App extends React.PureComponent {
+  render() {
+    return (
+      <div className={styles.container}>
+        <Cards />
+        <Chart />
+        <CountryPicker />
+      </div>
+    );
+  }
 }
 
 export default App;
