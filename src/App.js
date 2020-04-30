08cefb4ba6655f98dbc0262ec7451e1c991b1fcb
Elvis Serrão <elvis.serrao1@gmail.com>
2020-04-30 00:49:27 -0300

    Use async componentDidMount to fetch data

--- src/App.js
+++ src/App.js
@@ -3,16 +3,22 @@ import { Cards, Chart, CountryPicker } from './components';
 import styles from './App.module.css';
 import fetchData from './api';
 
-class App extends React.PureComponent {
+class App extends React.Component {
+  state = {
+    data: {},
+  };
+
   async componentDidMount() {
-    const data = await fetchData();
-    console.log(data);
+    const fetchedData = await fetchData();
+    this.setState({ data: fetchedData });
   }
 
   render() {
+    const { data } = this.state;
+
     return (
       <div className={styles.container}>
-        <Cards />
+        <Cards data={data} />
         <Chart />
         <CountryPicker />
       </div>
