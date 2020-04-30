d24263f509ac8a8681d829ce7072584d4af24a7d
Elvis Serrão <elvis.serrao1@gmail.com>
2020-04-30 00:48:26 -0300

    Return only response's attributes that matter

--- src/api/index.js
+++ src/api/index.js
@@ -4,8 +4,11 @@ const url = 'https://covid19.mathdro.id/api';
 
 const fetchData = async () => {
   try {
-    const response = await axios.get(url);
-    return response;
+    const {
+      data: { confirmed, recovered, deaths, lastUpdate },
+    } = await axios.get(url);
+
+    return { confirmed, recovered, deaths, lastUpdate };
   } catch (error) {
     return null;
   }
