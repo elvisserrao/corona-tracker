253d9a789bc2b65cc6e459891b3e01e58defc586
Elvis Serrão <elvis.serrao1@gmail.com>
2020-05-08 18:39:25 -0300

    Create basic card to show fetched data

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,7 +1,26 @@
 import React from 'react';
+import { Card, CardContent, Typography, Grid } from '@material-ui/core';
+import styles from './Cards.module.css';
 
-const Cards = () => {
-  return <h1>Cards</h1>;
+// eslint-disable-next-line react/prop-types
+const Cards = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
+  if (!confirmed) return 'Loading...';
+  return (
+    <div className={styles.container}>
+      <Grid container spacing={3} justify='center'>
+        <Grid item component={Card}>
+          <CardContent>
+            <Typography color='textSecondary' gutterBottom>
+              Infected
+            </Typography>
+            <Typography variant='h5'>{confirmed.value}</Typography>
+            <Typography color='textSecondary'>{lastUpdate}</Typography>
+            <Typography variant='body2'>Number of active cases of COVID-19</Typography>
+          </CardContent>
+        </Grid>
+      </Grid>
+    </div>
+  );
 };
 
 export default Cards;
