b784c8ddb8bdb516feec544bf76f7a63c664b7b3
Elvis Serrão <elvis.serrao1@gmail.com>
2020-05-16 00:27:12 -0300

    Fetch daily data

--- src/api/index.js
+++ src/api/index.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const url = 'https://covid19.mathdro.id/api';
 
-const fetchData = async () => {
+export const fetchData = async () => {
   try {
     const {
       data: { confirmed, recovered, deaths, lastUpdate },
@@ -14,4 +14,16 @@ const fetchData = async () => {
   }
 };
 
-export { fetchData as default };
+export const fetchDailyData = async () => {
+  try {
+    const { data } = await axios.get(`${url}/daily`);
+
+    const modifiedData = data.map((dailyData) => ({
+      confirmed: dailyData.confirmed.total,
+      deaths: dailyData.deaths.total,
+      date: dailyData.reportDate,
+    }));
+
+    return modifiedData;
+  } catch (error) {}
+};
